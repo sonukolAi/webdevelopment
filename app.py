eb37e677488d7dca49d50b09aa4966e5cbf69002
sonukolAi <136089232+sonukolAi@users.noreply.github.com>
2023-06-10 09:37:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,30 +7,23 @@ PORT = 3330
 
 @app.route('/<num>')
 def get_train_status(num):
-    try:
-        url = f"https://www.google.com/search?q={num}+train+running+status"
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    #      https://trainstatus.com/runningstatus/{num}  .panel-heading
+    #      https://spotyourtrain.com/trainstatus?train={num} .table-success  
+    url = f"https://runningstatus.in/status/{num}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        train_name = soup.find('head').text.replace('Live Train Running Status', '').strip()
+        current_station = soup.find(class_='table-success').text.strip()
+        
+        data = {
+            'Name': train_name,
+            'currentStation': current_station
         }
-        response = requests.get(url, headers=headers)
-        data = response.text
-        modified_data = modify_json(data)
-        return modified_data
-    except Exception as e:
-        print(e)
-        return 'Error occurred', 500
-
-def modify_json(data):
-    soup = BeautifulSoup(data, 'html.parser')
-    train_name = soup.select('div.k9rLYb')[0].text.strip()
-    live_status = soup.select('div.dK1Bub .rUtx7d')[1].text
-    delay_time = soup.select('div.Rjvkvf.MB86Dc')[1].text.strip()
-
-    return {
-        'trainName': train_name,
-        'liveStatus': live_status,
-        'delayTime': delay_time
-    }
+        return data
+    else:
+        return 'Error retrieving data', 500
 
 if __name__ == '__main__':
     app.run(port=PORT)
