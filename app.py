a3464e9d5d4c23bcd614c20e5a06911fc906969b
sonukolAi <136089232+sonukolAi@users.noreply.github.com>
2023-06-10 11:51:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
-import requests
-from flask import Flask
-
-app = Flask(__name__)
-PORT = 3330
+from selenium import webdriver
 
 @app.route('/<num>')
 def get_train_status(num):
     try:
         url = f"https://www.google.com/search?q={num}+train+running+status"
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
-        response = requests.get(url, headers=headers)
-        modified_html = modify_html(response.text)  # Modify the HTML response
+
+        # Use Selenium to perform the request
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')  # Run Chrome in headless mode
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        response = driver.page_source
+
+        modified_html = modify_html(response)  # Modify the HTML response
+        driver.quit()  # Quit the browser
+
         return modified_html
     except Exception as e:
         return f"Error: {str(e)}"
-
-def modify_html(html):
-    # Implement your logic to modify the HTML response here
-    # You can use libraries like BeautifulSoup to parse and manipulate the HTML
-    modified_html = html  # Placeholder, modify the HTML as per your requirements
-    return modified_html
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT)
